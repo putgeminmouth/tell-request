2f4a4dd132af0ba5070200c8c94b14dca0abd806
putgeminmouth <4316458+putgeminmouth@users.noreply.github.com>
2022-11-04 22:53:23 -0400

    Add explicit focus for nav shortcuts
    Fixes #7

--- src/l10n/en-US.js
+++ src/l10n/en-US.js
@@ -85,6 +85,9 @@ export const strings = {
             closeButton: {
                 text: 'Close'
             }
+        },
+        shortcutsButton: {
+            title: 'Click this to temporarily enable navigation shortcuts'
         }
     }
 };
