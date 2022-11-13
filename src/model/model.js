623a223e83dda0205d90e2a6d71a894ead6f025f
putgeminmouth <4316458+putgeminmouth@users.noreply.github.com>
2022-11-12 19:03:46 -0500

    Split things up a little into files and such

---
+++ src/model/model.js
@@ -0,0 +1,155 @@
+'use strict';
+
+import { Util } from '../common.js';
+
+export class Ids {
+    static _idGen = 0;
+    static initId(id) {
+        this._idGen = ++id;
+    }
+    static nextId() { return `${++Ids._idGen}`; }
+    static valid(id) { return !!id; } // documentation
+}
+
+class Visual {
+    constructor({ id, context }) {
+        this.id = id || Ids.nextId();
+        this.context = context;
+    }
+
+    static import(data) {
+        const typeKey = Object.keys(data)[0];
+        switch (typeKey) {
+            case 'comment': return Comment.import(data);
+        }
+    }
+
+    export() {
+        return {
+            id: this.id,
+            ...this.context.export()
+        };
+    }
+}
+
+export class Comment extends Visual {
+    constructor({ id, context, text }) {
+        super({ id, context });
+        this.text = text || '';
+    }
+
+    static import(data) {
+        const { id, text } = data.comment;
+        const context = FileContext.import(data.comment);
+        return new Comment({ id, context, text });
+    }
+
+    export() {
+        return {
+            comment: {
+                ...super.export(),
+                text: this.text
+            }
+        };
+    }
+}
+
+export class File {
+
+    constructor(filename) {
+        this.filename = filename;
+    }
+
+    static import(data) {
+        const { filename } = data.file;
+        return new File(filename);
+    }
+
+    export() {
+        return { file: { filename: this.filename } };
+    }
+}
+
+export class FileContext {
+    constructor({ file, lineNo }) {
+        this.file = file;
+        this.lineNo = lineNo;
+    }
+
+    static import(data) {
+        const { lineNo } = data.context;
+        const file = File.import(data.context);
+        return new FileContext({ file, lineNo });
+    }
+
+    export() {
+        return {
+            context: {
+                lineNo: this.lineNo,
+                ...this.file.export()
+            }
+        };
+    }
+}
+
+export class Presentation {
+    constructor() {
+        this.visuals = [];
+        this.events = Util.createEventTarget();
+    }
+
+    addOrReplaceVisual({ visual, position }) {
+        const existing = this.visuals.findIndex(x => x.id === visual.id);
+        let removed = [];
+        if (existing !== -1) {
+            removed = this.visuals.splice(existing, 1);
+        } else {
+        }
+        this.visuals.splice(isNaN(position) ? this.visuals.length : position, 0, visual);
+
+        this.events.dispatchEvent(new CustomEvent('change', { detail: { added: [visual], removed: removed } }));
+    }
+
+    removeVisual({ id }) {
+        const existing = this.visuals.findIndex(x => x.id === id);
+        if (existing === -1) return;
+
+        const removed = this.visuals.splice(existing, 1);
+
+        this.events.dispatchEvent(new CustomEvent('change', { detail: { added: [], removed: removed } }));
+    }
+
+    moveVisual({ id, position }) {
+        const visual = this.visuals.find(x => x.id === id);
+        const oldIndex = this.indexOf({ id });
+
+        this.visuals.splice(oldIndex, 1);
+        this.visuals.splice(position, 0, visual);
+
+        this.events.dispatchEvent(new CustomEvent('change', { detail: { added: [visual], removed: [visual] } }));
+    }
+
+    findByLineNo(filename, lineNo) {
+        return this.visuals.find(x => x.context.file.filename === filename && x.context.lineNo === lineNo);
+    }
+
+    indexOf({ id }) {
+        if (id) return this.visuals.findIndex(x => x.id === id);
+        return -1;
+    }
+
+    export() {
+        return { visuals: this.visuals.map(x => x.export()) };
+    }
+
+    import(data) {
+        const removed = this.visuals;
+
+        const added = data.visuals.map(v => {
+            const vv = Visual.import(v);
+            return vv;
+        });
+        this.visuals = added;
+        this.events.dispatchEvent(new CustomEvent('change', { detail: { added, removed } }));
+    }
+}
---
+++ src/pull.js
@@ -0,0 +1,235 @@
+'use strict';
+
+import { GithubApi, PullRequestPage } from './github.js';
+import { MAGIC, Opt, Promises, Try, Util, Element } from './common.js';
+import { Comment, File, FileContext, Ids, Presentation } from './model/model.js';
+import { CommentUI, DividerUI, SidebarUI } from './ui/ui.js';
+
+const parseComment = c => {
+    const PARSE_REGEX = new RegExp(`^(?<before>.*?)(?:<!--\\s*)?(?:${MAGIC})(?<data>.*)(?:${MAGIC})(?:\\s*-->)?(?<after>.*)$`, 'gs');
+    let { value: matches } = c.matchAll(PARSE_REGEX)?.next();
+    if (!matches)
+        return;
+
+    const body = matches.groups.data;
+    const rest = matches.groups.before + matches.groups.after;
+    const j = Try(() => JSON.parse(body));
+    if (!j) return;
+    return {
+        data: j,
+        comment: rest
+    };
+};
+
+const renderComment = c => {
+    return c.comment + ((!c.comment.length || /\s$/.test(c.comment)) ? '' : '\n') + '<!-- ' + MAGIC + JSON.stringify(c.data, null, 2) + MAGIC + ' -->';
+};
+
+class App {
+    constructor({ github, prPage }) {
+        this.github = github;
+        this.prPage = prPage;
+        this.events = Util.createEventTarget();
+        this.presentation = new Presentation();
+
+        this.files = [];
+
+        this.presentation.events.addEventListener('change', e => this.onPresentationChange(e));
+        this.events.addEventListener('select', e => this.onSelect(e));
+    }
+
+    async init(document) {
+        const sidebar = this.sidebar = new SidebarUI();
+        document.querySelector('[data-target="diff-layout.mainContainer"].Layout-main').after(sidebar.rootElem);
+
+        const divider = this.divider = new DividerUI();
+        divider.events.addEventListener('resize', e => this.onDividerResize(e));
+        divider.events.addEventListener('collapse', e => this.onDividerCollapse(e));
+        sidebar.rootElem.before(divider.rootElem);
+
+        sidebar.events.addEventListener('navTo', e => this.onSidebarNav(e));
+        sidebar.events.addEventListener('delete', e => this.onSidebarDelete(e));
+        sidebar.events.addEventListener('reorder', e => this.onSidebarReorder(e));
+        sidebar.events.addEventListener('save', e => this.onSidebarSave(e));
+
+        this.initAddVisualButtons();
+
+        const issueForm = await this.github.issue.fetchIssueEditForm(this.prPage.getPullId());
+        const currentValue = issueForm.editForm.querySelector('textarea').value;
+        const parsed = parseComment(currentValue);
+        this.import(parsed.data);
+    }
+
+    import(data) {
+        const getAllIds = (o) => {
+            if (o?.id) return o?.id;
+            return Object.values(o).flatMap(v => {
+                if (Array.isArray(v))
+                    return v.flatMap(getAllIds);
+                if (typeof (v) === 'object')
+                    return getAllIds(v);
+                return [];
+            });
+        };
+
+        const maxId = getAllIds(data).map(x => parseInt(x)).toArray().sort((a, b) => b - a).first();
+        Ids.initId(maxId);
+        this.presentation.import(data);
+    }
+
+    export() {
+        return this.presentation.export();
+    }
+
+    async persist() {
+        const currentValue = (await this.github.issue.fetchIssueEditForm(this.prPage.getPullId())).editForm.querySelector('textarea').value;
+        const currentParsed = parseComment(currentValue) || { comment: currentValue, data: {} };
+        currentParsed.data = this.export();
+        await this.github.issue.updateIssuePart({ part: 'body', text: renderComment(currentParsed) });
+    }
+
+    createCommentUI({ fileElem, value }) {
+        const commentUI = new CommentUI({ github: this.github, prPage: this.prPage, fileElem, value });
+        commentUI.events.addEventListener('accept', async e => {
+            const { comment } = e.detail;
+            this.presentation.addOrReplaceVisual({ visual: comment });
+        });
+
+        commentUI.rootElem.addEventListener('focusin', _ => {
+            this.selectVisual(value.id);
+        });
+        commentUI.rootElem.addEventListener('click', _ => {
+            this.selectVisual(value.id);
+        });
+        return commentUI;
+    }
+
+    initAddVisualButtons() {
+
+        const createButton = ({ file, fileElem, originalButton }) => {
+            const lineNo = originalButton.ancestors('td').first().previousElementSibling.getAttribute('data-line-number');
+            const context = new FileContext({ file, lineNo });
+
+            const addButton = document.createElement('button');
+            addButton.classList.add('add-line-comment');
+            addButton.classList.add('btn-link');
+            addButton.innerHTML = originalButton.innerHTML;
+            addButton.style.backgroundColor = 'red';
+            addButton.style.left = '20px';
+            originalButton.parentElement.prepend(addButton)
+
+            addButton.addEventListener('click', async e => {
+                const commentUI = this.createCommentUI({ fileElem, value: new Comment({ context }) });
+
+                originalButton.ancestors().filter(x => x.tagName === 'TR').first()
+                    .after(commentUI.rootElem);
+                commentUI.writeButton.click();
+            });
+            return addButton;
+        };
+
+        const fileElems = document.querySelectorAll('div[data-tagsearch-path].file');
+        fileElems.forEach(fileElem => {
+            const file = new File(fileElem.getAttribute('data-tagsearch-path'));
+            fileElem.querySelectorAll('button.add-line-comment')
+                .forEach(originalButton => createButton({ file, fileElem, originalButton }));
+        });
+    }
+
+    findVisualUI(id) {
+        return document.querySelector(`[data-visual-id="${id}"].visual-root`)?.data.visualUI;
+    }
+
+    onPresentationChange(e) {
+        const { added, removed } = e.detail;
+        removed.forEach(x => this.sidebar.remove(x.id));
+        removed.forEach(x => document.querySelector(`[data-visual-id="${x.id}"].visual-root`)?.remove());
+
+        added.forEach(x => this.sidebar.add(x, this.presentation.indexOf({ id: x.id })));
+        added.forEach(x => {
+            const tr = document.querySelector(`div.file .diff-table tr:has(td[data-line-number="${x.context.lineNo}"])`);
+            const fileElem = tr.ancestors().find(x => x.classList.contains('file')).first();
+            const commentUI = this.createCommentUI({ fileElem, value: x })
+            tr.after(commentUI.rootElem);
+            commentUI.setPreviewTab(); // no need to await
+        });
+    }
+
+    onSidebarNav(e) {
+        const { id } = e.detail;
+        this.findVisualUI(id).rootElem.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        this.selectVisual(id);
+    }
+
+    onSidebarDelete(e) {
+        const { id } = e.detail;
+        this.presentation.removeVisual({ id });
+    }
+
+    async onSidebarReorder(e) {
+        const { id, newPosition } = e.detail;
+        this.presentation.moveVisual({ id, position: newPosition });
+        this.sidebar.move(id, newPosition);
+        await this.persist();
+    }
+
+    async onSidebarSave(e) {
+        const promise = e.detail.promise();
+        try {
+            await this.persist();
+        } finally {
+            promise.resolve();
+        }
+    }
+
+    onSelect(e) {
+        const { id } = e.detail;
+        this.sidebar.select(id);
+    }
+
+    selectVisual(id) {
+        if (this.findVisualUI(id).rootElem.classList.contains('selected')) return;
+        document.querySelectorAll(`.${MAGIC}.visual-root.selected`).forEach(x => x.classList.remove('selected'));
+        this.findVisualUI(id)?.rootElem.classList.add('selected');
+        this.events.dispatchEvent(new CustomEvent('select', { detail: { id } }));
+    }
+
+    onDividerResize(e) {
+        const { pageX } = e.detail;
+        this.sidebar.rootElem.style.width = `${this.sidebar.rootElem.getBoundingClientRect().right - pageX}px`;
+    }
+
+    onDividerCollapse(e) {
+        const { collapsed } = e.detail;
+        this.sidebar.rootElem.classList.toggle('collapsed', collapsed);
+    }
+}
+
+let app;
+
+document.addEventListener('readystatechange', async e => {
+    if (document.readyState !== 'complete') return;
+    {
+        const prPage = new PullRequestPage();
+        const {
+            owner, repository, pull
+        } = prPage.parseUrl();
+        const github = new GithubApi({
+            repositoryUrl: `/${owner}/${repository}`,
+            pullUrl: `/${owner}/${repository}/pull/${pull}`,
+        });
+        app = new App({ document, github, prPage });
+        await app.init(document);
+    }
+
+    (() => {
+        const debug = document.createElement('button');
+        debug.innerHTML = 'Debug';
+        debug.addEventListener('click', e => {
+            const exported = JSON.stringify(app.export());
+            navigator.clipboard.writeText(exported);
+            console.log(exported);
+        });
+        document.body.prepend(debug);
+    }).apply();
+});
